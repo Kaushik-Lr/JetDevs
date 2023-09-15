de7382367758fb4a3bc1011f0e92e0573f349032
lrtestdemo <lr.testdemo@gmail.com>
2023-09-15 18:55:25 +0530

    [Update] Code push on main branch

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './src/redux/store';
+import {StatusBar} from 'react-native';
+import {COLORS} from './src/styles';
+import AppRouter from './src/navigation';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StatusBar
+          barStyle="dark-content"
+          backgroundColor={COLORS.transparent}
+        />
+        <AppRouter />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ src/typings/Navigation.d.ts
@@ -0,0 +1,9 @@
+// need to add types when new screen is added
+export type MainStackParams = {
+    Home: undefined,
+    Favorite: undefined,
+} 
+
+export type AuthStackParams = {
+    Login : undefined,
+}
